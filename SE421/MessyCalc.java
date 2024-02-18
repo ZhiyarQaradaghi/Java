38292bcfb508dd6a3d3374047087356b2be32808
Lord <zb21012@auis.edu.krd>
2024-02-18 20:55:17 +0300

    My Lab 1 part 2

--- SE421/MessyCalc.java
+++ SE421/MessyCalc.java
@@ -1,97 +1,154 @@
 package SE421;
 import java.util.*;
 
-
 public class MessyCalc {
-    public static int x;
-    public static int y;
-    Scanner sc = new Scanner(System.in);
-    public static void main(String...args) {
-        
-        MessyCalc obj = new MessyCalc();
-        
-        
-        int arrget[] = obj.GetNumbers(x, y);
-        
-        System.out.println("The first number is = "+arrget[0]);
-        System.err.println("The second number is = "+arrget[1]);
-                
-        int arr[] = new int[80];
-        Addition(arr, arrget[0], arrget[1]);
-        printAddition(arrget[0], arrget[1]);
-        arr = new int[80];
-        Multiplication(arr, arrget[0], arrget[1]);
-        printMultiplication(arrget[0], arrget[1]);
-        arr = new int[80];
-        Subtraction(arr, arrget[0], arrget[1]);
-        printSubtraction(arrget[0], arrget[1]);
+    
+    static Scanner sc = new Scanner(System.in);
+    private static int x;
+    private static int y;
 
+    public static void main(String... args) {
+        
+        MessyCalc obj = new MessyCalc(0, 0);
+        obj.enterNumbers();
+
+        int[] arrget = obj.getBothNumbers(x, y);
+
+        System.out.println("The first number is = " + arrget[0]);
+        System.out.println("The second number is = " + arrget[1]);
+
+        String operationCases = SelectingAnOperation.selectOperation();
+        Operations operations = new Operations();
+        switch (operationCases) {
+
+            case "addition":
+                operations.performAddition(arrget[0], arrget[1]);
+                break;
+                
+            case "multiplication":
+                operations.performMultiplication(arrget[0], arrget[1]);
+                break;
+
+            case "subtraction":
+                operations.performSubtraction(arrget[0], arrget[1]);
+                break;
+
+            default:
+                System.out.println("Wrong Choice");
+        }
     }
 
-    public int[] GetNumbers(int x, int y) {
-        int arrget[] = new int[2];
+    public void enterNumbers() {
         System.out.println("Enter the first number = ");
         x = sc.nextInt();
         System.out.println("Enter the second number = ");
         y = sc.nextInt();
-        arrget[0] = x;
-        arrget[1] = y;
-        return arrget;
-    }
-    
-
-
-
-    public static void Addition(int arr[], int x, int y) {
-        int index = 0;
-        for (int i = 0; i < 20; i++) {
-            arr[index++] = x+y;
-            
-        }
     }
 
-    public static void printAddition(int x, int y) {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("Addition : "+(x+y));
-            
-        }
+    public int[] getBothNumbers(int x, int y) {
+        int[] arr = new int[2];
+        arr[0] = x;
+        arr[1] = y;
+        return arr;
     }
 
-    public static void Multiplication(int arr[], int x, int y) {
-        int index = 0;
-        for (int i = 0; i < 10; i++) {
-            arr[index++] = x*y;
-            
-        }
+    public boolean setFirstNumber(int z) {
+        this.x = z;
+        return true;
     }
 
-    public static void printMultiplication(int x, int y) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Multiplication : "+(x*y));
-            
-        }
+    public boolean setSecondNumber(int v) {
+        this.y = v;
+        return true;
     }
 
-
-    public static void Subtraction(int arr[], int x, int y) {
-        int index = 0;
-        for (int i = 0; i < 8; i++) {
-            arr[index++] = x-y;
-            
-        }
+    public MessyCalc(int x, int y) {
+        setFirstNumber(x);
+        setSecondNumber(y);
     }
+}
 
-    public static void printSubtraction(int x, int y) {
-        for (int i = 0; i < 8; i++) {
-            System.out.println("Subtraction : "+(x-y));
-            
-        }
-    }
+class Operations { 
 
+    private Addition addition;
+    private Multiplication multiplication;
+    private Subtraction subtraction;
 
-
-
-    
-
+    public Operations() {
         
+        addition = new Addition();
+        multiplication = new Multiplication();
+        subtraction = new Subtraction();
+
+    }
+
+    public void performAddition(int x, int y) {
+        addition.addingTwoNumbers(x, y);
+
+        addition.printAddition(x, y);
+    }
+
+    public void performMultiplication(int x, int y) {
+        multiplication.productOfTwoNumbers(x, y);
+
+        multiplication.printMultiplication(x, y);
+    }
+
+    public void performSubtraction(int x, int y) {
+        subtraction.subtraction(x, y);
+
+        subtraction.printSubtraction(x, y);
+    }
+}
+
+class Addition {
+
+    public void addingTwoNumbers(int x, int y){}
+
+    public void printAddition(int x, int y) {
+        System.out.println("The sum of both numbers is = " +(x+y));
+    }
+}
+
+class Multiplication {
+    
+    public void productOfTwoNumbers(int x, int y){}
+
+    public void printMultiplication(int x, int y) {
+        System.out.println("The product of both numbers is  = "+(x*y));
+    }
+}
+
+class Subtraction {
+
+    public void subtraction(int x, int y){}
+
+    public void printSubtraction(int x, int y) {
+        System.out.println("The difference of both numbers is = "+(x-y));
+    }
+}
+
+class SelectingAnOperation {
+
+    public static String selectOperation() {
+
+        System.out.println("Choose an operation = ");
+        System.out.println("1- Addition");
+        System.out.println("2- Multiplication");
+        System.out.println("3- Subtraction");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                return "addition";
+            case 2:
+                return "multiplication";
+            case 3:
+                return "subtraction";
+            default:
+                return null;
+        }
+    }
 }

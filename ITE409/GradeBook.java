969e7c66dca7d644459b0c3984dc060e7914b157
Lord <zb21012@auis.edu.krd>
2024-02-18 10:03:05 +0300

    indirectly change variable data through get and set methods through private access modifier

--- ITE409/GradeBook.java
+++ ITE409/GradeBook.java
@@ -1,8 +1,9 @@
 package ITE409;
 public class GradeBook {
-    int courseGrade;
-    static int year;
-    String courseName;
+    private int courseGrade;
+    private static int year = 2024; // Hiding --> Abstract / Trying to abstract pieces from the system / Encapsulate and Abstraction
+    String courseName; // whenever you have variables, always make these variables PRIVATE and have get and set methods 
+    // you can change the variable name freely without affecting the other methods and classes that use it
 
 
     // we will talk about access modifiers week 3 #1
@@ -10,12 +11,38 @@ public class GradeBook {
     // 4 types of access modifiers : 1- Public, 2- Private, 3- Protected, 4- Default
     /** Academia POV - everything is important / Industry POV : Public and Private are most important, default useless, avoid using Protected unless design issue -- many programming language removed those 2
      * Public - builtin java keyword
-     * Private - builtin java keyword
+     * Private - builtin java keyword / complete opposite of public / noone can access that given member except me!
      * Protected - builtin ....
-     * Default - nothing
+     * Default - anyone who are in the current package can access the class
      */
-    GradeBook(int grade) {
-        courseGrade = grade;
+
+    
+
+    // have a constructor to force people to enter a grade
+    public GradeBook(int courseGrade) {
+        setGrade(courseGrade); // call the method instead of modifying the coursegrade directly
+    }
+
+
+     // reading the value of course gradebook, instead of calling the variable itself now they can call the method
+     // traditionally the name starts with get for reading 
+    public int getGrade() {
+        return courseGrade;
+    }
+
+    public void setGrade(int g) {
+        if(g>=0 && g<=100) {
+            this.courseGrade = g; // why put this here? its a good habit and wont make a difference, but this means just the current object, imagine if we have an argument that has the same exact name as the variable grade
+        }else{
+            System.err.println("The grade should be between 0 and 100");
+        }
+        
+    }
+
+
+    public void print() {
+        System.out.println(this.year); // just always put this. to avoid problems like same arguments, its called variable over shadowing, where you want to use the instance variable instead of the arguments in the method
+        System.out.println(this.courseGrade);
     }
 
 }
--- ITE409/Ite409.java
+++ ITE409/Ite409.java
@@ -1,41 +1,21 @@
-package ITE409;
-/**  Java recap
-// every method has to be in a class
-// execute two commands for compiler, javac ./It409.java 
-// java will rewrite your application as byte code, you cannot read byte code files - after this step the ide will run the command java to start up the java vm which will look at your system specs and translate byte code to machine code 
-// machine code will be different on a mac os compared to a windows 
-// compile it once and run it everywhere
-// *** topic - some languages will go from compiler and immediately into machine code - making the application slightly faster
-a static method can only be with a static method
-curly brackets are used to set a scope, any variables declared in a scope will stay in that scope (local variables)
-*/
+package ITE409; 
 
-public class Ite409 { 
-    public static void main(String...args) /** you can write the square bracket in the main method as ... or []*/ { 
-        int x = 10;
-        int y = 5;
-        System.out.println(print(x)); // whenever you see  () then you are dealing with a method, when you have a method without {} hat means you are callng a method, but if it is with {} means you are declaring a method
-        System.out.println(calc(x,y));
-        //x; if there is a symbol but no () right after that means it is a variable 
-        //x();  this is dealing with a method 
-        // there are 2 variables so far - system . out -- out is a variable 
-        // there is a class called system and there is a variable called out and a method named println - giving 3 instructions to java
-        // sometimes you wont get the intended the resut, just stop the application and run it again
-    }   
 
-        public static int print(int value){ // void means this method does not return a value to the one who calls 
-            System.out.println(value);
-            return value*20; // whenever you return a value the code will immediately go back to the method caller so if you put it in a loop it will cancel it - can only be returned once
-        }
-        // continue means skip the loop
-        // break means stop the entire loop
-        public static int calc(int x, int y) {
-            int z = x + y;
-            int i = 0;
-            /**while (i < 10 || 1==1) {
-                System.out.println(i);
-                i = i*10;
-            }*/
-            return z;
-        }
+public class Ite409 {
+    public static void main(String...args) {
+        GradeBook b1 = new GradeBook(50);
+       /** b1.courseGrade = 90;
+        System.out.println(b1.courseGrade);
+        b1.print(); // we can access a private variable if it is in a public method and you call the method
+        */
+        b1.setGrade(-90); // they can indirectly change the variable data
+        //b1.courseGrade = -110; // imagine an idiot put a negative number that isnt logical, so how can i deal with this? I will design my classes in such a way that my numbers start from 0 and max is 100 using access modifiers
+        // key problem is you are exposing this variable so make courseGrade private but now all values are not valid, so create 2 methods, one for retrieving data
+        System.out.println(b1.getGrade()); // now they can call the method instead of the variable itself
+
+        
+
+
+    }
+    
 }

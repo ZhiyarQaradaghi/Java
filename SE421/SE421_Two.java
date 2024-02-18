f350fb88f1cb2cdd45a9b69fd57e9bf65334f5c0
Lord <zb21012@auis.edu.krd>
2024-02-18 12:02:59 +0300

    Best way, approach #3 by passing by reference and declaring arrays in the methods

--- SE421/SE421_Two.java
+++ SE421/SE421_Two.java
@@ -12,29 +12,34 @@ public class SE421_Two {
         // More code structure added to system == slower application -- called Over-Engineering which is as bad as Under-Engineering because writing code takes time and performance hits 
         // Lab 1 part 2 == MAKE IT OVER ENGINEERED 
         
-        int arr[] = new int[70];
+        //int arr[] = new int[70];
         
       // if you pass by reference and call it | 2 | 4 | 6 | 8 | 98 |
       // best method - REMEMBER TO RESET THE ARRAY !!
-        printEvenNumbers(arr);
+
+        int[] evens = printEvens(); //passing by ref, caller doesnt have to remember 
+        int[] odds = printOddNumbers(); // as long as that index pointer is pointing at it, then java will not delete the data structure
+       /**  printEvenNumbers(arr);
         arr = new int[70];
       // Data will be corrupted  | 1 | 3 | ... | 98 |
         printOddNumbers(arr);
         arr = new int[70];
-        printNumbersDivisibleBySix(arr);
+        printNumbersDivisibleBySix(arr);*/
         
 
 
 
     }
     // static int arr[] = new int[70]; easiest way but if this is production environment and you have 2 people at same time execute print odd and even and you want to add numbers to the array then you will corrupt the data
-    public static void printEvenNumbers(int arr[]) { // this line is code structure, passing array into () and calling it in main method is the worst method 
+    public static int[] printEvens() { // this line is code structure, passing array into () and calling it in main method is the worst method 
+        int arr[] = new int[70];
         int index = 0;
         for(int i = 0;i<=100;i++) {
             if(i%2==0)
             arr[index++] = i;
             System.out.println("even number = "+i);
         }
+        return arr; // returning the reference
     }
 
    /**  public static int findEvenNumbers(int[] evenArray) {
@@ -45,13 +50,15 @@ public class SE421_Two {
         }
     }*/
 
-    public static void printOddNumbers(int arr[]) {
+    public static int[] printOddNumbers() {
+        int arr[] = new int[70];
         int index = 0;
         for(int i = 0;i<=100;i++) {
             if(i%2!=0) // different ways: i%2==1 <-- slight confusing what is 1?, !i%2==0 <-- obscure but it will be better to use this when working with boolean operations
             arr[index++] = i;
             System.out.println("odd number = "+i);
         }
+        return arr;
         
     }
 

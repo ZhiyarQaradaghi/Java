657de85a80125e7285b296e2f206326b35f9c127
Lord <zb21012@auis.edu.krd>
2024-02-19 11:15:50 +0300

    added some operations to the ssl

--- ITS350/SLList.java
+++ ITS350/SLList.java
@@ -5,7 +5,44 @@ public class SLList {
     Node head;
     Node tail;
     int size;
+    
+    public static void main(String...args) {
+        SLList list = new SLList();
+        list.addFirst(10);
+        list.addFirst(3);
+        list.addLast(5);
+        list.addLast(20);
+        list.addFirst(1);
+        list.display();
+        list.deleteFirst();
+        list.deleteLast();
+        list.deleteLast();
+        list.deleteLast();
+        list.display();
+    }
 
+    void display() {
+
+        if(head == null) {
+            System.err.println("The List is Empty");
+            return;
+        } Node temp = head;
+        while (temp!=null) {
+            System.err.println(temp.data+" ");
+            temp = temp.next;
+        }System.out.println();
+
+    }
+
+    void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
     
 
     void addFirst(int data) {
@@ -17,6 +54,45 @@ public class SLList {
             head = newNode;
         }
         size++;
-    } 
+    }
     
+    void deleteFirst() {
+        if (head==null) {
+            System.out.println("Empty");
+            return;
+        }
+        head = head.next;
+        size--;
+    }
+    
+    // delete an element at the end of the list
+    // you cannot say tail == null
+    // delete at position
+    // add at position
+    // what if we have only one node, how do we delete
+
+    void deleteLast() {
+
+        if (head==null) {
+            System.out.println("Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next!=null) {
+            temp = temp.next;
+
+        }  
+        
+        temp.next = null;
+        tail = temp;
+        size--; 
+    }
+
+    void deleteAtPos() {
+
+    }
+
+    void addAtFirst() {
+        
+    }
 }

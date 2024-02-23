778818a4b0a476c1a50c44a90d7e251da9dd9673
Lord <zb21012@auis.edu.krd>
2024-02-23 22:49:59 +0300

    Singly Linked List with all methods

--- ITS350/PracticeLinkedList/SSList.java
+++ ITS350/PracticeLinkedList/SSList.java
@@ -28,6 +28,15 @@ public class SSList {
         System.out.println("\n----Deleting Last Node----\n");
         ob1.deleteLastNode();
         ob1.display();
+        System.out.println("\n----Insert Node At End----\n");
+        ob1.insertEnd(5);
+        ob1.display();
+        System.out.println("\n----Delete At Specific Position----\n");
+        ob1.deleteSpecificNode(0);
+        ob1.display();
+        System.out.println("\n----INSERT At Specific Position----\n");
+        ob1.insertAtPosition(6, 2);
+        ob1.display();
 
         System.out.println(ob1.isEmpty());
     }
@@ -40,6 +49,59 @@ public class SSList {
         
 
     }
+
+    public void insertEnd(int data) {
+        Node newNode = new Node(data);
+        Node temp = first; // create a temp node
+        while (temp.next!=null) { // go to the last node that is pointing at null
+            temp = temp.next;
+        }
+
+        temp.next = newNode; // now assign newNode to the last node (temp.next)
+        size++;
+    }
+
+    public void insertAtPosition(int data, int position) {
+        
+        if (position < 0 || position > size) { // check position validity
+            System.err.println("Position is INVALID : "+position)
+        }
+       
+        Node newNode = new Node(data);
+      
+        if (first == null) { // if the list is empty, then set the first node to new node and increase size to 1
+          
+            first = newNode;
+            size++;
+            return;
+        }
+       
+        if (position == 0) { // if the position is 0, then we just write the code for addFirst()
+            newNode.next = first;
+            first = newNode;
+            size++;
+            return;
+        }
+      
+        Node temp = first;
+        // keep going until we reach end of list or the position
+        while (temp != null && position > 1) {
+            
+            temp = temp.next; // keep moving the temp node to the next
+            
+        }
+     
+        if (temp != null) {
+           
+            Node temp2 = temp.next; // create another node to save the next node of temp
+        
+            temp.next = newNode; // we will insert the newNode with the data to next temp, so we lose the original temp next thats why we needed temp2
+            
+            newNode.next = temp2; // link the temp2 to newNode.next to make it point to null
+           
+            size++;
+        }
+    }    
 
 
     public Node deletFirstNode() {
@@ -63,17 +125,48 @@ public class SSList {
         temp.next = null; // now temp.next is the last node, so we delete it by making it null
         tail = temp; // make the tail point to the new last node 
         size--;
-        
+    }
+
+    public void deleteSpecificNode(int position) {
+        if (first == null) {
+            System.err.println("EMPTY");
+            return;
+        }
+        Node temp = first;
+
+        if (position == 0) {
+            first = first.next;
+            return;
+        }
+
+        for (int i = 0; temp!=null && i < position - 1; i++) {
+            temp = temp.next;
+        }
+
+        if (temp == null || temp.next == null) {
+            System.err.println("Position entered is beyond the number of nodes");
+            return;
+        }
+
+        Node newNode = temp.next.next;
+
+        temp.next = newNode;
+
+        size--;
 
     }
 
-    public void display() {
+
+
+    public void display() { // remember we cannot display reverse in singly ll because we can only move in one direction
         Node current = first;
         while (current!=null) {
             current.displayNode();
             current = current.next;
         }System.out.println("--> null");
     }
+
+   
 
 
     

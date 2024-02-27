98f54e8cdc4f8c609b306c3728f2a461a6e5d58f
Lord <zb21012@auis.edu.krd>
2024-02-27 10:00:49 +0300

    v2 ITE409 interface

--- ITE409/Interface_chapter8/Person.java
+++ ITE409/Interface_chapter8/Person.java
@@ -1,6 +1,6 @@
 package ITE409.Interface_chapter8;
 
-public interface Person {
+public interface Person { // interface is like an adjective, provides a classification/categorizes for classes
     /**Allows to have declarations of methods
      * but unlike classes
      *  - it doesnt have variables
@@ -9,5 +9,7 @@ public interface Person {
      public void setName(String n); // in interfaces, default property for access modifiers are PUBLIC !
      public String getName();
      // ^^ every person should have setName and getName
+
+     // limitation, you can only access methods that exist in Person interface
 
 }
